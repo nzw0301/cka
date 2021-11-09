6b4b47352bc1a8dc274a9b1b702dcfc69fb444be
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2021-11-10 03:07:04 +0900

    apply black; fix bug

--- cka/cka.py
+++ cka/cka.py
@@ -2,7 +2,6 @@ import numpy as np
 from numba import jit
 
 
-@staticmethod
 @jit(nopython=True)
 def gram_linear(x: np.ndarray) -> np.ndarray:
     """Compute Gram (kernel) matrix for a linear kernel.
@@ -16,7 +15,6 @@ def gram_linear(x: np.ndarray) -> np.ndarray:
     return x.dot(x.T)
 
 
-@staticmethod
 @jit(nopython=True)
 def gram_rbf(x: np.ndarray, threshold: float = 1.0) -> np.ndarray:
     """Compute Gram (kernel) matrix for an RBF kernel.
@@ -33,12 +31,15 @@ def gram_rbf(x: np.ndarray, threshold: float = 1.0) -> np.ndarray:
     dot_products = x.dot(x.T)
     sq_norms = np.diag(dot_products)
 
-    sq_distances = -2. * dot_products + np.expand_dims(sq_norms, axis=1) + np.expand_dims(sq_norms, axis=0)
+    sq_distances = (
+        -2.0 * dot_products
+        + np.expand_dims(sq_norms, axis=1)
+        + np.expand_dims(sq_norms, axis=0)
+    )
     sq_median_distance = np.median(sq_distances)
-    return np.exp(-sq_distances / (2. * threshold ** 2 * sq_median_distance))
+    return np.exp(-sq_distances / (2.0 * threshold ** 2 * sq_median_distance))
 
 
-@staticmethod
 @jit(nopython=True)
 def _center_gram(gram: np.ndarray, unbiased: bool = False) -> np.ndarray:
     """Center a symmetric Gram matrix.
@@ -66,19 +67,19 @@ def _center_gram(gram: np.ndarray, unbiased: bool = False) -> np.ndarray:
 
         np.fill_diagonal(gram, 0)
         means = np.sum(gram, 0) / (n - 2)
-        means -= np.sum(means) / (2. * (n - 1))
+        means -= np.sum(means) / (2.0 * (n - 1))
         gram -= np.expand_dims(means, axis=1)
         gram -= np.expand_dims(means, axis=0)
         np.fill_diagonal(gram, 0)
     else:
         means = np.sum(gram, 0) / n
-        means -= np.sum(means) / n / 2.
+        means -= np.sum(means) / n / 2.0
         gram -= np.expand_dims(means, axis=1)
         gram -= np.expand_dims(means, axis=0)
 
     return gram
 
-@staticmethod
+
 @jit(nopython=True)
 def _hsic(gram_x: np.ndarray, gram_y: np.ndarray) -> float:
     # Note: To obtain HSIC, this should be divided by (n-1)**2 (biased variant) or
@@ -88,6 +89,7 @@ def _hsic(gram_x: np.ndarray, gram_y: np.ndarray) -> float:
     normalization_x = np.linalg.norm(gram_x)
     normalization_y = np.linalg.norm(gram_y)
     return scaled_hsic / (normalization_x * normalization_y)
+
 
 def gram_cka(gram_x: np.ndarray, gram_y: np.ndarray, debiased: bool = False) -> float:
     """Compute CKA.
@@ -101,26 +103,32 @@ def gram_cka(gram_x: np.ndarray, gram_y: np.ndarray, debiased: bool = False) -> 
     """
     for x in [gram_x, gram_y]:
         if not np.allclose(x, x.T):
-            raise ValueError('Input must be a symmetric matrix.')
+            raise ValueError("Input must be a symmetric matrix.")
 
     gram_x = _center_gram(gram_x, unbiased=debiased)
     gram_y = _center_gram(gram_y, unbiased=debiased)
 
-
     return _hsic(gram_x, gram_y)
 
-@staticmethod
+
 @jit(nopython=True)
-def _debiased_dot_product_similarity_helper(xty, sum_squared_rows_x, sum_squared_rows_y, squared_norm_x, squared_norm_y, n: int):
+def _debiased_dot_product_similarity_helper(
+    xty, sum_squared_rows_x, sum_squared_rows_y, squared_norm_x, squared_norm_y, n: int
+):
     """Helper for computing debiased dot product similarity (i.e. linear HSIC)."""
     # This formula can be derived by manipulating the unbiased estimator from
     # Song et al. (2007).
-    return xty - n / (n - 2.) * sum_squared_rows_x.dot(sum_squared_rows_y) + squared_norm_x * squared_norm_y / ((n - 1) * (n - 2))
+    return (
+        xty
+        - n / (n - 2.0) * sum_squared_rows_x.dot(sum_squared_rows_y)
+        + squared_norm_x * squared_norm_y / ((n - 1) * (n - 2))
+    )
 
 
-@staticmethod
 @jit(nopython=True)
-def feature_space_linear_cka(features_x: np.ndarray, features_y: np.ndarray, biased: bool = False) -> np.ndarray:
+def feature_space_linear_cka(
+    features_x: np.ndarray, features_y: np.ndarray, debiased: bool = False
+) -> np.ndarray:
     """Compute CKA with a linear kernel, in feature space.
 
     This is typically faster than computing the Gram matrix when there are fewer
@@ -153,13 +161,32 @@ def feature_space_linear_cka(features_x: np.ndarray, features_y: np.ndarray, bia
         squared_norm_y = np.sum(sum_squared_rows_y)
 
         dot_product_similarity = _debiased_dot_product_similarity_helper(
-            dot_product_similarity, sum_squared_rows_x, sum_squared_rows_y,
-            squared_norm_x, squared_norm_y, n)
-        normalization_x = np.sqrt(_debiased_dot_product_similarity_helper(
-            normalization_x ** 2, sum_squared_rows_x, sum_squared_rows_x,
-            squared_norm_x, squared_norm_x, n))
-        normalization_y = np.sqrt(_debiased_dot_product_similarity_helper(
-            normalization_y ** 2, sum_squared_rows_y, sum_squared_rows_y,
-            squared_norm_y, squared_norm_y, n))
+            dot_product_similarity,
+            sum_squared_rows_x,
+            sum_squared_rows_y,
+            squared_norm_x,
+            squared_norm_y,
+            n,
+        )
+        normalization_x = np.sqrt(
+            _debiased_dot_product_similarity_helper(
+                normalization_x ** 2,
+                sum_squared_rows_x,
+                sum_squared_rows_x,
+                squared_norm_x,
+                squared_norm_x,
+                n,
+            )
+        )
+        normalization_y = np.sqrt(
+            _debiased_dot_product_similarity_helper(
+                normalization_y ** 2,
+                sum_squared_rows_y,
+                sum_squared_rows_y,
+                squared_norm_y,
+                squared_norm_y,
+                n,
+            )
+        )
 
     return dot_product_similarity / (normalization_x * normalization_y)
