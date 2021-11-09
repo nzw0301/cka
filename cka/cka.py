c47841e57d27aca68dac615e3f63dfc2cd33fdf3
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2021-11-10 03:24:21 +0900

    use original implementation

--- cka/cka.py
+++ cka/cka.py
@@ -1,9 +1,21 @@
+"""
+The source code comes from
+https://colab.research.google.com/github/google-research/google-research/blob/master/representation_similarity/Demo.ipynb
+by Kornblith, Simon and Norouzi, Mohammad and Lee, Honglak and Hinton, Geoffrey.
+
+The modifications are as follows:
+
+1. Apply `black`
+2. Rename `center_gram` with `_center_gram`
+
+Note that when I apply `numba.jit(nopython=True)` to all functions,
+I could not make the code faster, so I decided to use the original code.
+"""
+
 import numpy as np
-from numba import jit
 
 
-@jit(nopython=True)
-def gram_linear(x: np.ndarray) -> np.ndarray:
+def gram_linear(x):
     """Compute Gram (kernel) matrix for a linear kernel.
 
     Args:
@@ -15,8 +27,7 @@ def gram_linear(x: np.ndarray) -> np.ndarray:
     return x.dot(x.T)
 
 
-@jit(nopython=True)
-def gram_rbf(x: np.ndarray, threshold: float = 1.0) -> np.ndarray:
+def gram_rbf(x, threshold=1.0):
     """Compute Gram (kernel) matrix for an RBF kernel.
 
     Args:
@@ -30,18 +41,12 @@ def gram_rbf(x: np.ndarray, threshold: float = 1.0) -> np.ndarray:
     """
     dot_products = x.dot(x.T)
     sq_norms = np.diag(dot_products)
-
-    sq_distances = (
-        -2.0 * dot_products
-        + np.expand_dims(sq_norms, axis=1)
-        + np.expand_dims(sq_norms, axis=0)
-    )
+    sq_distances = -2 * dot_products + sq_norms[:, None] + sq_norms[None, :]
     sq_median_distance = np.median(sq_distances)
-    return np.exp(-sq_distances / (2.0 * threshold ** 2 * sq_median_distance))
+    return np.exp(-sq_distances / (2 * threshold ** 2 * sq_median_distance))
 
 
-@jit(nopython=True)
-def _center_gram(gram: np.ndarray, unbiased: bool = False) -> np.ndarray:
+def _center_gram(gram, unbiased=False):
     """Center a symmetric Gram matrix.
 
     This is equivalent to centering the (possibly infinite-dimensional) features
@@ -50,38 +55,50 @@ def _center_gram(gram: np.ndarray, unbiased: bool = False) -> np.ndarray:
     Args:
         gram: A num_examples x num_examples symmetric matrix.
         unbiased: Whether to adjust the Gram matrix in order to compute an unbiased
-        estimate of HSIC. Note that this estimator may be negative.
+            estimate of HSIC. Note that this estimator may be negative.
 
     Returns:
         A symmetric matrix with centered columns and rows.
     """
-
+    if not np.allclose(gram, gram.T):
+        raise ValueError("Input must be a symmetric matrix.")
     gram = gram.copy()
-    n = gram.shape[0]
 
     if unbiased:
         # This formulation of the U-statistic, from Szekely, G. J., & Rizzo, M.
         # L. (2014). Partial distance correlation with methods for dissimilarities.
         # The Annals of Statistics, 42(6), 2382-2412, seems to be more numerically
         # stable than the alternative from Song et al. (2007).
-
+        n = gram.shape[0]
         np.fill_diagonal(gram, 0)
-        means = np.sum(gram, 0) / (n - 2)
-        means -= np.sum(means) / (2.0 * (n - 1))
-        gram -= np.expand_dims(means, axis=1)
-        gram -= np.expand_dims(means, axis=0)
+        means = np.sum(gram, 0, dtype=np.float64) / (n - 2)
+        means -= np.sum(means) / (2 * (n - 1))
+        gram -= means[:, None]
+        gram -= means[None, :]
         np.fill_diagonal(gram, 0)
     else:
-        means = np.sum(gram, 0) / n
-        means -= np.sum(means) / n / 2.0
-        gram -= np.expand_dims(means, axis=1)
-        gram -= np.expand_dims(means, axis=0)
+        means = np.mean(gram, 0, dtype=np.float64)
+        means -= np.mean(means) / 2
+        gram -= means[:, None]
+        gram -= means[None, :]
 
     return gram
 
 
-@jit(nopython=True)
-def _hsic(gram_x: np.ndarray, gram_y: np.ndarray) -> float:
+def cka(gram_x, gram_y, debiased=False):
+    """Compute CKA.
+
+    Args:
+        gram_x: A num_examples x num_examples Gram matrix.
+        gram_y: A num_examples x num_examples Gram matrix.
+        debiased: Use unbiased estimator of HSIC. CKA may still be biased.
+
+    Returns:
+        The value of CKA between X and Y.
+    """
+    gram_x = _center_gram(gram_x, unbiased=debiased)
+    gram_y = _center_gram(gram_y, unbiased=debiased)
+
     # Note: To obtain HSIC, this should be divided by (n-1)**2 (biased variant) or
     # n*(n-3) (unbiased variant), but this cancels for CKA.
     scaled_hsic = gram_x.ravel().dot(gram_y.ravel())
@@ -91,29 +108,8 @@ def _hsic(gram_x: np.ndarray, gram_y: np.ndarray) -> float:
     return scaled_hsic / (normalization_x * normalization_y)
 
 
-def gram_cka(gram_x: np.ndarray, gram_y: np.ndarray, debiased: bool = False) -> float:
-    """Compute CKA.
-
-    Args:
-        gram_x: A num_examples x num_examples Gram matrix.
-        gram_y: A num_examples x num_examples Gram matrix.
-
-    Returns:
-        The value of CKA between X and Y.
-    """
-    for x in [gram_x, gram_y]:
-        if not np.allclose(x, x.T):
-            raise ValueError("Input must be a symmetric matrix.")
-
-    gram_x = _center_gram(gram_x, unbiased=debiased)
-    gram_y = _center_gram(gram_y, unbiased=debiased)
-
-    return _hsic(gram_x, gram_y)
-
-
-@jit(nopython=True)
 def _debiased_dot_product_similarity_helper(
-    xty, sum_squared_rows_x, sum_squared_rows_y, squared_norm_x, squared_norm_y, n: int
+    xty, sum_squared_rows_x, sum_squared_rows_y, squared_norm_x, squared_norm_y, n
 ):
     """Helper for computing debiased dot product similarity (i.e. linear HSIC)."""
     # This formula can be derived by manipulating the unbiased estimator from
@@ -125,10 +121,7 @@ def _debiased_dot_product_similarity_helper(
     )
 
 
-@jit(nopython=True)
-def feature_space_linear_cka(
-    features_x: np.ndarray, features_y: np.ndarray, debiased: bool = False
-) -> np.ndarray:
+def feature_space_linear_cka(features_x, features_y, debiased=False):
     """Compute CKA with a linear kernel, in feature space.
 
     This is typically faster than computing the Gram matrix when there are fewer
@@ -138,25 +131,23 @@ def feature_space_linear_cka(
         features_x: A num_examples x num_features matrix of features.
         features_y: A num_examples x num_features matrix of features.
         debiased: Use unbiased estimator of dot product similarity. CKA may still be
-        biased. Note that this estimator may be negative.
+            biased. Note that this estimator may be negative.
 
     Returns:
         The value of CKA between X and Y.
     """
-
-    n = features_x.shape[0]
-
-    features_x = features_x - np.expand_dims(np.sum(features_x, 0), axis=0) / n
-    features_y = features_y - np.expand_dims(np.sum(features_y, 0), axis=0) / n
+    features_x = features_x - np.mean(features_x, 0, keepdims=True)
+    features_y = features_y - np.mean(features_y, 0, keepdims=True)
 
     dot_product_similarity = np.linalg.norm(features_x.T.dot(features_y)) ** 2
     normalization_x = np.linalg.norm(features_x.T.dot(features_x))
     normalization_y = np.linalg.norm(features_y.T.dot(features_y))
 
     if debiased:
+        n = features_x.shape[0]
         # Equivalent to np.sum(features_x ** 2, 1) but avoids an intermediate array.
-        sum_squared_rows_x = np.sum(features_x ** 2, 1)
-        sum_squared_rows_y = np.sum(features_y ** 2, 1)
+        sum_squared_rows_x = np.einsum("ij,ij->i", features_x, features_x)
+        sum_squared_rows_y = np.einsum("ij,ij->i", features_y, features_y)
         squared_norm_x = np.sum(sum_squared_rows_x)
         squared_norm_y = np.sum(sum_squared_rows_y)
 
