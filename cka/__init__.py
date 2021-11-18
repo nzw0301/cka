50b40a96017d1d6eff847c362879ba2c2cc0ae70
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2021-11-18 15:17:07 +0900

    split funcs

--- cka/__init__.py
+++ cka/__init__.py
@@ -1,1 +1,5 @@
 __version__ = "0.1.0"
+
+from cka.cka import IncrementalCKA
+from cka.cka import cka, feature_space_linear_cka
+from cka.gram import gram_linear, gram_rbf
--- cka/cka.py
+++ cka/cka.py
@@ -13,76 +13,7 @@ I could not make the code faster, so I decided to use the original code.
 """
 
 import numpy as np
-
-
-def gram_linear(x):
-    """Compute Gram (kernel) matrix for a linear kernel.
-
-    Args:
-        x: A num_examples x num_features matrix of features.
-
-    Returns:
-        A num_examples x num_examples Gram matrix of examples.
-    """
-    return x.dot(x.T)
-
-
-def gram_rbf(x, threshold=1.0):
-    """Compute Gram (kernel) matrix for an RBF kernel.
-
-    Args:
-        x: A num_examples x num_features matrix of features.
-        threshold: Fraction of median Euclidean distance to use as RBF kernel
-        bandwidth. (This is the heuristic we use in the paper. There are other
-        possible ways to set the bandwidth; we didn't try them.)
-
-    Returns:
-        A num_examples x num_examples Gram matrix of examples.
-    """
-    dot_products = x.dot(x.T)
-    sq_norms = np.diag(dot_products)
-    sq_distances = -2 * dot_products + sq_norms[:, None] + sq_norms[None, :]
-    sq_median_distance = np.median(sq_distances)
-    return np.exp(-sq_distances / (2 * threshold ** 2 * sq_median_distance))
-
-
-def _center_gram(gram, unbiased=False):
-    """Center a symmetric Gram matrix.
-
-    This is equivalent to centering the (possibly infinite-dimensional) features
-    induced by the kernel before computing the Gram matrix.
-
-    Args:
-        gram: A num_examples x num_examples symmetric matrix.
-        unbiased: Whether to adjust the Gram matrix in order to compute an unbiased
-            estimate of HSIC. Note that this estimator may be negative.
-
-    Returns:
-        A symmetric matrix with centered columns and rows.
-    """
-    if not np.allclose(gram, gram.T):
-        raise ValueError("Input must be a symmetric matrix.")
-    gram = gram.copy()
-
-    if unbiased:
-        # This formulation of the U-statistic, from Szekely, G. J., & Rizzo, M.
-        # L. (2014). Partial distance correlation with methods for dissimilarities.
-        # The Annals of Statistics, 42(6), 2382-2412, seems to be more numerically
-        # stable than the alternative from Song et al. (2007).
-        n = gram.shape[0]
-        np.fill_diagonal(gram, 0)
-        means = np.sum(gram, 0, dtype=np.float64) / (n - 2)
-        means -= np.sum(means) / (2 * (n - 1))
-        gram -= means[:, None]
-        gram -= means[None, :]
-        np.fill_diagonal(gram, 0)
-    else:
-        means = np.mean(gram, 0, dtype=np.float64)
-        means -= np.mean(means) / 2
-        gram -= means[:, None]
-        gram -= means[None, :]
-
-    return gram
+from cka.gram import gram_linear, _center_gram, gram_rbf
 
 
 def cka(gram_x, gram_y, debiased=False):
@@ -255,7 +186,7 @@ class IncrementalCKA:
         np.fill_diagonal(gram_x, 0)
         np.fill_diagonal(gram_y, 0)
 
-        # since computing terms used in the denom of cka can be skipped,
+        # since computing terms used in the denom of cka is duplicated,
         # we only compute them when either layer's index is 0.
         if index_feature_x == 0 or index_feature_y == 0:
             if index_feature_x == 0:
@@ -269,7 +200,7 @@ class IncrementalCKA:
         """
         Compute mini-batch CKA defined by Eq. 2 in the original paper.
 
-        Returns: `np.darray` whose element is cka. The shape is `num_layers_0` \times `num_layers_1`.
+        Returns: `np.ndarray` whose element is cka. The shape is `num_layers_0` \times `num_layers_1`.
 
         """
         K = np.min(self._num_mini_batches)
